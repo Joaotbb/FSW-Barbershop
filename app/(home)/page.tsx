e5a2a2e670a181ac11cbe0f0d6d6ceb1a1926e8d
Kraze <joaotbarros9@gmail.com>
2024-01-31 18:52:17 +0000

    feat: added welcome message

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,7 +1,17 @@
 import Header from "../_components/header";
+import { format } from "date-fns";
+import { pt } from "date-fns/locale/pt";
 
 export default function Home() {
   return <div>
     <Header/>
+    <div className="px-5 pt-5">
+        <h2 className="text-xl font-bold">Olá, Miguel!</h2>
+        <p className="capitalize text-sm">
+          {format(new Date(), "EEEE',' dd 'de' MMMM", {
+            locale: pt,
+          })}
+        </p>
+      </div>
   </div>;
 }
