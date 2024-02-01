dd822648fd405c9bc8784f8c28712741f5e5a664
Kraze <joaotbarros9@gmail.com>
2024-02-01 19:17:54 +0000

    feat: side-menu added

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -1,16 +1,28 @@
+"use client";
+
 import Image from "next/image";
 import { Card, CardContent } from "./ui/card";
 import { Button } from "./ui/button";
 import { MenuIcon } from "lucide-react";
+import { Sheet, SheetContent, SheetTrigger } from "./ui/sheet";
+import SideMenu from "./side-menu";
 
 const Header = () => {
   return (
     <Card>
-      <CardContent className="p-5 justify-between  itens-center flex flex-row">
-        <Image alt="FSW barber" src="/logo.png" height={18} width={120} />
-        <Button variant="outline" size="icon" className="h-8 w-8">
-            <MenuIcon/>
-        </Button>
+      <CardContent className="p-5 justify-between items-center flex flex-row">
+        <Image src="/logo.png" alt="FSW Barber" height={18} width={120} />
+        <Sheet>
+          <SheetTrigger asChild>
+            <Button variant="outline" size="icon">
+              <MenuIcon size={16} />
+            </Button>
+          </SheetTrigger>
+
+          <SheetContent className="p-0">
+            <SideMenu />
+          </SheetContent>
+        </Sheet>
       </CardContent>
     </Card>
   );
