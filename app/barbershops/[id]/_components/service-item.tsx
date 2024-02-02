17c485dd699aa28b4e61cbefb2112095d1c611f4
Kraze <joaotbarros9@gmail.com>
2024-02-02 20:07:16 +0000

    feat: calendar added

--- app/barbershops/[id]/_components/service-item.tsx
+++ app/barbershops/[id]/_components/service-item.tsx
@@ -1,10 +1,20 @@
 "use client";
 
 import { Button } from "@/app/_components/ui/button";
+import { Calendar } from "@/app/_components/ui/calendar";
 import { Card, CardContent } from "@/app/_components/ui/card";
+import {
+  Sheet,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/app/_components/ui/sheet";
 import { Service } from "@prisma/client";
+import {} from "lucide-react";
 import { signIn } from "next-auth/react";
 import Image from "next/image";
+import { useState } from "react";
 
 interface ServiceItemProps {
   service: Service;
@@ -12,6 +22,8 @@ interface ServiceItemProps {
 }
 
 const ServiceItem = ({ service, isAuthenticated }: ServiceItemProps) => {
+  const [date, setDate] = useState<Date | undefined>(new Date());
+
   const handleBookingClick = () => {
     if (!isAuthenticated) {
       return signIn("google");
@@ -45,9 +57,52 @@ const ServiceItem = ({ service, isAuthenticated }: ServiceItemProps) => {
                   currency: "EUR",
                 }).format(Number(service.price))}
               </p>
-              <Button variant="secondary" onClick={handleBookingClick}>
-                Reservar
-              </Button>
+              <Sheet>
+                <SheetTrigger asChild>
+                  <Button variant="secondary" onClick={handleBookingClick}>
+                    Reservar
+                  </Button>
+                </SheetTrigger>
+
+                <SheetContent className="p-0">
+                  <SheetHeader className="text-left px-5 py-6 border-b border-solid border-secondary">
+                    <SheetTitle>Fazer Reserva</SheetTitle>
+                  </SheetHeader>
+
+                  <div className="py-8">
+                  <Calendar
+                      mode="single"
+                      selected={date}
+                      // prevent from schedule on days that have gone
+                      fromDate={new Date()}
+                      styles={{
+                        head_cell: {
+                          width: "100%",
+                          textTransform: "capitalize",
+                        },
+                        cell: {
+                          width: "100%",
+                        },
+                        button: {
+                          width: "100%",
+                        },
+                        nav_button_previous: {
+                          width: "32px",
+                          height: "32px",
+                        },
+                        nav_button_next: {
+                          width: "32px",
+                          height: "32px",
+                        },
+                        caption: {
+                          textTransform: "capitalize",
+                        },
+                      }}
+                    />
+                  </div>
+
+                </SheetContent>
+              </Sheet>
             </div>
           </div>
         </div>
